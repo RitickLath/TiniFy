ded54755bab0b97a6cdfa947619b82afb8cca327
RitickLath <ritick943lath@gmail.com>
2025-04-30 03:53:08 +0530

    Project Setup + Basic Routing + Not Found Page

---
+++ frontend/src/App.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+import { Home, Dashboard, Login, Signup, NotFound } from "./Sections/Section";
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+};
+
+export default App;
