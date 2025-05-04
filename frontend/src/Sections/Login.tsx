925d7273247e47b625e8f1e923f5586bb1501678
RitickLath <ritick943lath@gmail.com>
2025-05-04 18:53:50 +0530

    Navbar to Register routes

--- frontend/src/Sections/Login.tsx
+++ frontend/src/Sections/Login.tsx
@@ -1,8 +1,14 @@
 import { FC } from "react";
 import Form from "../Components/Form";
+import Navbar from "../Components/Navbar";
 
 const Login: FC = () => {
-  return <Form isSignup={false} />;
+  return (
+    <div>
+      <Navbar />
+      <Form isSignup={false} />
+    </div>
+  );
 };
 
 export default Login;
--- frontend/src/Sections/Signup.tsx
+++ frontend/src/Sections/Signup.tsx
@@ -1,8 +1,10 @@
 import Form from "../Components/Form";
+import Navbar from "../Components/Navbar";
 
 const Signup = () => {
   return (
     <div>
+      <Navbar />
       <Form isSignup={true} />
     </div>
   );
